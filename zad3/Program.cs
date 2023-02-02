b082ecb27d034a7f53be5a95efbef8e046707885
Elena <lenkabeloglazova@yandex.ru>
2023-02-02 12:41:46 +0300

    add zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите номер недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 5) ;
+{
+    Console.Write(" это не выходной");
+
+ if (num >= 6 && num <= 7)
+{
+    Console.Write(" Да, это выходной");
+}
+else
+ Console.Write(" Не существует такого дня недели");
+}
