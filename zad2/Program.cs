e3f0b6ae8b2ce1778b5caaa3494810b224180e4b
Elena <lenkabeloglazova@yandex.ru>
2023-02-02 12:24:35 +0300

    add zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+int num2 = num / 10;
+int num3 = num2 % 10;
+
+Console.WriteLine(num3);
+
+if(num3==num3)
+{
+int num4 = num3 / 100;
+int num5 =num4 % 10;
+Console.WriteLine(num3);
+}
